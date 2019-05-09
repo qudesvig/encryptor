945e416052d36e63812714a0e778245de77d6fd0
Quentin DESVIGNES <qudesvig@e1r10p13.42.fr>
2019-05-09 05:07:39 +0200

    cost 858

--- srcs/evolve.c
+++ srcs/evolve.c
@@ -6,7 +6,7 @@
 /*   By: qudesvig <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 13:34:39 by qudesvig          #+#    #+#             */
-/*   Updated: 2019/05/07 00:43:18 by qudesvig         ###   ########.fr       */
+/*   Updated: 2019/05/09 04:49:53 by qudesvig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ double		*evolve_weights(int n, double *base, int comeback, int nb_change)
 
 	lr = 1;
 	if (comeback > 10)
-		lr = 0.05;
+		lr = 0.05 / 16;
 	else
-		lr = 0.2;
+		lr = 0.1 / 16;
 	j = 0;
 	i = n % (NB_WEIGHT - nb_change);
 	while (j < nb_change && i + j < NB_WEIGHT && i + j > 0)
 	{
-		ch = rand_dbl((-1 * lr * nb_change), (1 * lr * nb_change));
+		ch = rand_dbl((-1 * lr), (1 * lr));
 		base[i + j] += ch;
 		j++;
 	}
@@ -43,14 +43,14 @@ double		*evolve_bias(int n, double *base, int comeback, int nb_change)
 
 	lr = 2;
 	if (comeback > 10)
-		lr = 0.05;
+		lr = 0.05 / 8;
 	else
-		lr = 0.2;
+		lr = 0.1 / 8;
 	j = 0;
 	i = n % (NB_BIAS - nb_change);
 	while (j < nb_change && i + j < NB_BIAS && i + j > 0)
 	{
-		base[i + j] += rand_dbl(-1 * lr * nb_change, 1 * lr * nb_change);
+		base[i + j] += rand_dbl(-1 * lr, 1 * lr);
 		j++;
 	}
 	return (base);
